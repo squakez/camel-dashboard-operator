5ddd8460e988c9ff062262c971c27db3ba390d08
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2026-09-09 09:57:01 +0200

    chore(e2e): verify alternative endpoints

--- e2e/common/integration_test.go
+++ e2e/common/integration_test.go
@@ -112,6 +112,18 @@ func TestVerifyCamelKIntegrationTimerToLog(t *testing.T) {
 				),
 			)
 
+			// Verify health status is UP
+			g.Eventually(
+				CamelMonitorStatus(t, ctx, ns, "timer-to-log"),
+				TestTimeoutMedium,
+			).Should(
+				WithTransform(
+					func(s v1alpha1.CamelMonitorStatus) bool {
+						return isCamelMonitorHealthStatusUP(s)
+					},
+					BeTrue(),
+				),
+			)
 			// We check the success rate is not reporting weird results
 			g.Eventually(
 				CamelMonitorStatus(t, ctx, ns, "timer-to-log"),
@@ -119,35 +131,102 @@ func TestVerifyCamelKIntegrationTimerToLog(t *testing.T) {
 			).Should(
 				WithTransform(
 					func(s v1alpha1.CamelMonitorStatus) bool {
-						sr := s.SuccessRate
-						if sr == nil {
-							return false
-						}
-						// We need tolerance as the check may be slightly higher than 1 minute,
-						//ie, time to reconcile.
-						tolerance := *sr.SamplingIntervalDuration / 10
-						elapsed := time.Since(sr.LastTimestamp.Time)
-						if elapsed >= *sr.SamplingIntervalDuration+tolerance {
-							return false
-						}
-						if sr.SamplingIntervalFailed != 0 {
-							return false
-						}
-						if sr.SamplingIntervalTotal <= 0 {
-							return false
-						}
-						if sr.Status != "Success" {
-							return false
-						}
-						if sr.SuccessPercentage != "100.00" {
-							return false
-						}
-
-						return true
+						return isCamelMonitorMetricsHealthy(s)
 					},
 					BeTrue(),
 				),
 			)
 		})
 	})
+}
+
+func TestVerifyQuarkusDefaultAlternativeEndpoints(t *testing.T) {
+	WithNewTestNamespace(t, func(ctx context.Context, g *WithT, ns string) {
+		// Test a simple route with indefinite message delivery (all success)
+		t.Run("all success messages", func(t *testing.T) {
+			ExpectExecSucceed(t, g,
+				exec.Command(
+					"kubectl",
+					"apply",
+					"-f",
+					"files/timer-to-log-default-quarkus.yaml",
+					"-n",
+					ns,
+				),
+			)
+
+			// Verify health status is UP (on /q/health)
+			g.Eventually(
+				CamelMonitorStatus(t, ctx, ns, "timer-to-log-default-quarkus"),
+				TestTimeoutMedium,
+			).Should(
+				WithTransform(
+					func(s v1alpha1.CamelMonitorStatus) bool {
+						isUp := isCamelMonitorHealthStatusUP(s)
+						quarkusHealthEndpoint := len(s.Pods) > 0 && s.Pods[0].ObservabilityService.HealthEndpoint == "q/health"
+
+						return isUp && quarkusHealthEndpoint
+					},
+					BeTrue(),
+				),
+			)
+			// We check the /q/metrics are read fine
+			g.Eventually(
+				CamelMonitorStatus(t, ctx, ns, "timer-to-log-default-quarkus"),
+				TestTimeoutMedium,
+			).Should(
+				WithTransform(
+					func(s v1alpha1.CamelMonitorStatus) bool {
+						isHealthy := isCamelMonitorMetricsHealthy(s)
+						quarkusMetricsEndpoint := len(s.Pods) > 0 && s.Pods[0].ObservabilityService.MetricsEndpoint == "q/metrics"
+
+						return isHealthy && quarkusMetricsEndpoint
+					},
+					BeTrue(),
+				),
+			)
+		})
+	})
+}
+
+func isCamelMonitorMetricsHealthy(s v1alpha1.CamelMonitorStatus) bool {
+	sr := s.SuccessRate
+	if sr == nil {
+		return false
+	}
+
+	// We need tolerance as the check may be slightly higher than 1 minute,
+	// ie, time to reconcile.
+	tolerance := *sr.SamplingIntervalDuration / 10
+	elapsed := time.Since(sr.LastTimestamp.Time)
+	if elapsed >= *sr.SamplingIntervalDuration+tolerance {
+		return false
+	}
+
+	if sr.SamplingIntervalFailed != 0 {
+		return false
+	}
+	if sr.SamplingIntervalTotal <= 0 {
+		return false
+	}
+	if sr.Status != "Success" {
+		return false
+	}
+	if sr.SuccessPercentage != "100.00" {
+		return false
+	}
+
+	return true
+}
+
+func isCamelMonitorHealthStatusUP(s v1alpha1.CamelMonitorStatus) bool {
+	if len(s.Pods) == 0 {
+		return false
+	}
+
+	if s.Pods[0].Runtime.Status != "UP" {
+		return false
+	}
+
+	return true
 }
